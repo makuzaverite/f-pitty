d2864187ce4ff90a7acfb0dfb0e8698a7f93e78c
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-02-01 18:33:14 +0200

    fix: ls initial done

--- pkg/commands/list.go
+++ pkg/commands/list.go
@@ -1,6 +1,10 @@
 package commands
 
 import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
 	"github.com/spf13/cobra"
 )
 
@@ -10,11 +14,30 @@ var listCmd = &cobra.Command{
 	Short: "List all files in a directory",
 	Long:  "Listing all files in the directory",
 	Run: func(cmd *cobra.Command, args []string) {
-
+		handleListingFiles()
 	},
 }
 
 // listCmd.Flags().IntVarP(1,"list","")
 func init() {
 	rootCommand.AddCommand(listCmd)
+}
+
+func handleListingFiles() {
+	currDir, err := os.Getwd()
+
+	if err != nil {
+		fmt.Println("Failed to open this directory")
+		os.Exit(1)
+	}
+
+	files, err := ioutil.ReadDir(currDir)
+
+	for _, file := range files {
+		fmt.Print(file.Name() + "  ")
+	}
+}
+
+func isDir(file os.FileInfo) bool {
+	return file.IsDir()
 }
